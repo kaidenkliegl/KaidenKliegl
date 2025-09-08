ffebdf99bb5e3ae803289da19bc78ed6b7413ca4
Kaiden Kliegl <132409071+kaidenkliegl@users.noreply.github.com>
2025-09-08 00:07:22 -0700

    Fix BrowserRouter basename for GitHub Pages

--- src/index.js
+++ src/index.js
@@ -3,18 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import ThemeContextProvider from './contexts/ThemeContext'
-
+import ThemeContextProvider from './contexts/ThemeContext';
+import { BrowserRouter } from 'react-router-dom';  // 👈 import BrowserRouter
 
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 
 ReactDOM.render(
-    <ThemeContextProvider>
-      <App />
-    </ThemeContextProvider>,
+  <React.StrictMode>
+    <BrowserRouter basename="/developer-portfolio">  {/* 👈 set basename */}
+      <ThemeContextProvider>
+        <App />
+      </ThemeContextProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
   document.getElementById('root')
 );
-
 
 reportWebVitals();

df191441047981184c16f186b52d8c2544152b78
Kaiden Kliegl <132409071+kaidenkliegl@users.noreply.github.com>
2025-09-07 23:50:11 -0700

    updated some personal info

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,11 +1,7 @@
 import one from '../assets/svg/projects/one.svg'
 import two from '../assets/svg/projects/two.svg'
-import three from '../assets/svg/projects/three.svg'
-import four from '../assets/svg/projects/four.svg'
-import five from '../assets/svg/projects/five.svg'
-import six from '../assets/svg/projects/six.svg'
-import seven from '../assets/svg/projects/seven.svg'
-import eight from '../assets/svg/projects/eight.svg'
+
+import houseLift from '../assets/png/booking.png'
 
 
 export const projectsData = [
@@ -20,81 +16,83 @@ export const projectsData = [
     },
     {
         id: 2,
-        projectName: 'Railway Tracking',
-        projectDesc: 'This project is a railway tracking system that allows users to obtain relevant information about train timing.',
-        tags: ['Django', 'Tailwind CSS'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        projectName: 'IdeaVault',
+        projectDesc: 'IdeaVault is a note-taking and idea management app where users can create, organize, and store their thoughts, notes, and projects in one place.',
+        tags: ['React', 'Express', 'PostgreSQL', 'TailwindCSS'],
+        code: 'https://github.com/your-username/ideavault',
+        demo: 'https://ideavault-demo-link.com',
         image: two
     },
+    
     {
         id: 3,
-        projectName: 'Weather App',
-        projectDesc: 'Weather forecast systems and applications predict weather conditions based on multiple parameters.',
-        tags: ['Django', 'CSS', 'Material Ui'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: three
+        projectName: 'HouseLift',
+        projectDesc: 'HouseLift is a full-stack Airbnb clone that allows users to list, discover, and book vacation rentals with authentication, reviews, and bookings.',
+        tags: ['React', 'Redux', 'Express', 'PostgreSQL', 'TailwindCSS'],
+        code: 'https://github.com/your-username/houselift',
+        demo: 'https://houselift-demo-link.com',
+        image: houseLift
     },
-    {
-        id: 4,
-        projectName: 'Android Patient Tracker',
-        projectDesc: 'This project involves the development of an Android application for viewing and managing patient data.',
-        tags: ['Flutter', 'Firebase'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: four
-    },
-    {
-        id: 5,
-        projectName: 'E-Commerce App',
-        projectDesc: 'A Simple E-commerce application',
-        tags: ['React Native', 'Firebase'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: five
-    },
-    {
-        id: 6,
-        projectName: 'Uber Lite',
-        projectDesc: 'Uber clone',
-        tags: ['Flutter'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: six
-    },
-    {
-        id: 7,
-        projectName: 'Stock Market App',
-        projectDesc: 'A simple stock market API app',
-        tags: ['React', 'Redux', 'Bootstrap'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: seven
-    },
-    {
-        id: 8,
-        projectName: 'Car Pooling System',
-        projectDesc: 'The carpooling system merges multiple people in a car which leads to meet new people, reduces pollution',
-        tags: ['Flutter', 'React'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: eight
-    },
+    
+//     {
+//         id: 4,
+//         projectName: 'Android Patient Tracker',
+//         projectDesc: 'This project involves the development of an Android application for viewing and managing patient data.',
+//         tags: ['Flutter', 'Firebase'],
+//         code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+//         demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+//         image: four
+//     },
+//     {
+//         id: 5,
+//         projectName: 'E-Commerce App',
+//         projectDesc: 'A Simple E-commerce application',
+//         tags: ['React Native', 'Firebase'],
+//         code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+//         demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+//         image: five
+//     },
+//     {
+//         id: 6,
+//         projectName: 'Uber Lite',
+//         projectDesc: 'Uber clone',
+//         tags: ['Flutter'],
+//         code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+//         demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+//         image: six
+//     },
+//     {
+//         id: 7,
+//         projectName: 'Stock Market App',
+//         projectDesc: 'A simple stock market API app',
+//         tags: ['React', 'Redux', 'Bootstrap'],
+//         code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+//         demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+//         image: seven
+//     },
+//     {
+//         id: 8,
+//         projectName: 'Car Pooling System',
+//         projectDesc: 'The carpooling system merges multiple people in a car which leads to meet new people, reduces pollution',
+//         tags: ['Flutter', 'React'],
+//         code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+//         demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+//         image: eight
+//     },
 ]
 
 
-// Do not remove any fields.
-// Leave it blank instead as shown below
+// // Do not remove any fields.
+// // Leave it blank instead as shown below
 
-/* 
-{
-    id: 1,
-    projectName: 'Car Pooling System',
-    projectDesc: '',
-    tags: ['Flutter', 'React'],
-    code: '',
-    demo: '',
-    image: ''
-}, 
-*/
+// /* 
+// {
+//     id: 1,
+//     projectName: 'Car Pooling System',
+//     projectDesc: '',
+//     tags: ['Flutter', 'React'],
+//     code: '',
+//     demo: '',
+//     image: ''
+// }, 
+// */
--- src/data/socialsData.js
+++ src/data/socialsData.js
@@ -1,6 +1,6 @@
 export const socialsData = {
-    github: 'https://github.com/',
-    linkedIn: 'https://www.linkedin.com/in',
+    github: 'https://github.com/kaidenkliegl',
+    linkedIn: 'https://www.linkedin.com/jobs/?mcid=6994434350142418944&src=go-pa&trk=sem-ga_campid.18853522261_asid.146084015209_crid.732549151385_kw.linkedin_d.c_tid.kwd-296170574619_n.g_mt.e_geo.1014257&cid=&gclsrc=aw.ds&gad_source=1&gad_campaignid=18853522261&gbraid=0AAAAABIeiSo4l9Ttkc37UYymUmEkb2RzK&gclid=CjwKCAjw2vTFBhAuEiwAFaScwodflDsz3sZHyET6CfXhfMTBI99inZA4rWCrg9jWYmfzaQNRNJoTCRoCJbIQAvD_BwE',
     instagram: 'https://www.instagram.com/',
     twitter: 'https://twitter.com/',
 }
